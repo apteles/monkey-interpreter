70c64db2a91089aa253bd14a1e24336f87f6d0e4
André Teles <andre.telestp@gmail.com>
2025-09-13 18:33:42 -0300

    add repl

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/apteles/monkey-interpreter/repl"
+)
+
+func main() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Println("Feel free to type in commands")
+	repl.Start(os.Stdin, os.Stdout)
+
+}
